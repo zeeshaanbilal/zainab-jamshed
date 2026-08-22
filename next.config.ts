cebe5ab9288d286dbb44769f8ee80ac23372000d
awais4813 <awais4813@users.noreply.github.com>
2026-08-22 14:46:28 +0500

    fix blogs

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverActions: {
-    bodySizeLimit: '10mb',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
   },
 };
 
