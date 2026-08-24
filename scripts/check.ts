74783f87f6308e103ae8ab1af2ab7d3d9a0321df
awais4813 <awais4813@users.noreply.github.com>
2026-08-24 13:19:56 +0500

    craete slug

--- scripts/check.ts
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-prisma.blog.findMany().then(blogs => {
-  console.log(JSON.stringify(blogs, null, 2));
-}).finally(() => {
-  prisma.$disconnect();
-});
